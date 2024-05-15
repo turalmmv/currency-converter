3c80f43f12f242127702b88770bfdb6937754c8e
elcinehmedovv <elchinahmadoff@gmail.com>
2024-05-15 17:03:44 +0400

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let amount = 0;
 let btn = document.querySelectorAll(".based button");
 let btn2 = document.querySelectorAll(".target button");
 let targetInput = document.querySelector('.target input[type="text"]')
+let basedInput = document.querySelector('.based input[type="text"]')
+
 btn.forEach(element => {
     element.addEventListener("click", () => {
         basedCurrency = element.innerText;
@@ -13,6 +15,31 @@ btn.forEach(element => {
     });
 });
 
+function colorChoosing(element){
+    let button = element.currentTarget.querySelectorAll("button");
+    button.forEach(e=>{
+       e.style.backgroundColor = ""; 
+    })
+    const clickedItem = element.target.closest("button");
+    if(clickedItem){
+        clickedItem.style.backgroundColor = "rgba(131, 58, 224, 1)";
+    }
+
+}
+
+const first = document.querySelector(".converter .based");
+first.addEventListener("click",colorChoosing);
+
+
+const second = document.querySelector(".converter .target");
+second.addEventListener("click",colorChoosing);
+
+// btn2.forEach(element => {
+//     element.addEventListener("click", () => {
+//         basedCurrency = element.innerText;
+//         fetchConversation()
+//     });
+// });
 btn2.forEach(element => {
     element.addEventListener("click", () => {
         targetCurrency = element.innerText;
